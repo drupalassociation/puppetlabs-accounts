5647f4ab1f4e6b6298c5d01b84eafb68013a19ca
Rudy Grigar <basic@drupal.org>
2016-01-26 17:48:56 -0800

    Add require

--- lib/puppet/functions/options_parser.rb
+++ lib/puppet/functions/options_parser.rb
@@ -1,6 +1,7 @@
 # Parse an ssh authorized_keys option string into an array using its expected
 # pattern which matches a CSV. The pattern should be a string.
 
+require 'csv'
 
 Puppet::Functions.create_function(:options_parser) do
   dispatch :options_parser_String do
