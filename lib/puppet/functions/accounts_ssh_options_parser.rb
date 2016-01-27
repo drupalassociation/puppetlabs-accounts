db097a617c6c90146fed19d4d34376ba03199a97
Rudy Grigar <basic@drupal.org>
2016-01-26 19:46:31 -0800

    This is crazy, but it just might work.

--- lib/puppet/functions/accounts_ssh_options_parser.rb
+++ lib/puppet/functions/accounts_ssh_options_parser.rb
@@ -1,7 +1,6 @@
 # Parse an ssh authorized_keys option string into an array using its expected
-# pattern which matches a CSV. The pattern should be a string.
-
-require 'csv'
+# pattern which matches a crazy regex slightly modified from shellwords. The
+# pattern should be a string.
 
 Puppet::Functions.create_function(:accounts_ssh_options_parser) do
   dispatch :accounts_ssh_options_parser_String do
@@ -9,6 +8,19 @@ Puppet::Functions.create_function(:accounts_ssh_options_parser) do
   end
 
   def accounts_ssh_options_parser_String(str)
-    CSV.parse_line([str].to_csv)
+    words = []
+    field = ''
+    str.scan(/\G\,*(?>([^\,\\\'\"]+)|'([^\']*)'|("(?:[^\"\\]|\\.)*")|(\\.?)|(\S))(\,|\z)?/m) do
+      |word, sq, dq, esc, garbage, sep|
+      raise ArgumentError, "Unmatched double quote: #{str.inspect}" if garbage
+      field << (word || sq || dq || esc.gsub(/\\(.)/, '\\1'))
+      field.gsub(/\\=/,'\\=\"')
+      if sep
+        words << field
+        field = ''
+      end
+    end
+    words
   end
+
 end
